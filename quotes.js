e4f2730ba5a6f3c17dad2dcf076ad35c18971bf0
Anna Michaelsen <anna.michaelsen@symfoni.com>
2019-08-22 17:34:26 +0200

    Add random source

--- quotes.js
+++ quotes.js
@@ -2,26 +2,33 @@ const quoteTag = document.querySelector('h1')
 const authorTag = document.querySelector('p')
 const randomTag = document.getElementById('randomize')
 
-let data = []
-
-// Let's load in the real data
-fetch('https://api.superhi.com/api/test/quotes')
-  .then(response => response.json())
-  .then(jsonData => {
-    data = jsonData
-    getQuote()
-  })
+// let data = []
+//
+// // Let's load in the real data
+// fetch('https://api.superhi.com/api/test/quotes')
+//   .then(response => response.json())
+//   .then(jsonData => {
+//     data = jsonData
+//     getQuote()
+//   })
 
 const getQuote = function () {
-  if (data.length > 0) {
-    // Get a random quote
-    const randomNumber = Math.floor(Math.random() * data.length)
-    const randomQuote = data[randomNumber]
+//   if (data.length > 0) {
+//     // Get a random quote
+//     const randomNumber = Math.floor(Math.random() * data.length)
+//     const randomQuote = data[randomNumber]
+//
+//     // Get a random quote and insert in on the site
+//     quoteTag.innerHTML = randomQuote.quote
+//     authorTag.innerHTML = randomQuote.author
+//   }
 
-    // Get a random quote and insert in on the site
-    quoteTag.innerHTML = randomQuote.quote
-    authorTag.innerHTML = randomQuote.author
-  }
+  fetch('https://api.superhi.com/api/test/quotes/random')
+    .then(response => response.json())
+    .then(jsonData => {
+      quoteTag.innerHTML = jsonData.quote
+      authorTag.innerHTML = jsonData.author
+    })
 }
 
 // Run getQuote on click of randomize
